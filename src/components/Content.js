a1924bff19c6d0ed7e32e339e37d13db166d4907
GoranAviani <goranaviani@gmail.com>
2022-05-17 19:21:05 +0200

    ex2

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,12 +1,24 @@
 import React from 'react';
-
+import './Content.css'
 
 const Content = () => {
 
     return (
-        <div>
-            content
-        </div>
+        <body>
+            <div class="info">
+                <div class="infoTitle">
+                    This is Info Title
+                </div>
+                <div class="infoText">
+                    This is a lot of info text that is supposed to be displayed here, so here is more text
+                    lets hope there is enoug of text here
+                </div>
+                <div class="buttons">
+                    <button class="black">Black button</button>
+                    <button class="transparent">Transparent button</button>
+                </div>
+            </div>
+        </body>
     )
 }
 
