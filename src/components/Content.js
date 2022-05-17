0323ee295cff2a63b2e773e9e86552746405acb8
GoranAviani <goranaviani@gmail.com>
2022-05-17 19:40:24 +0200

    ex2

--- src/components/Content.js
+++ src/components/Content.js
@@ -11,6 +11,7 @@ const Content = () => {
                 </div>
                 <div class="infoText">
                     This is a lot of info text that is supposed to be displayed here, so here is more text
+                    lets hope there is enoug of text here lets hope there is enoug of text here lets hope there is enoug of text here
                     lets hope there is enoug of text here
                 </div>
                 <div class="buttons">
