aa9662c317e211a994bc57715359694250150538
GoranAviani <goranaviani@gmail.com>
2022-05-17 18:23:22 +0200

    ex2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import React from 'react';
-
+import './Navbar.css'
 
 const Navbar = () => {
 
     return (
-        <div>
-            footer
+        <div class="navbar">
+            <div class="">
+                <h2>logo</h2>
+                <button>nav 1</button>
+                <button>nav 2 is this</button>
+                <button>nav 3</button>
+                <button>nav 4 here</button>
+                <button>nav 5</button>
+            </div>
+            <div class="right">
+                <button>log in</button>
+                <button>sign up here</button>
+            </div>
         </div>
     )
 }
