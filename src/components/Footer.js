c48a7478befc1d96b57c731d86e98b1e948c6f42
GoranAviani <goranaviani@gmail.com>
2022-05-17 19:41:16 +0200

    ex2

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,8 +9,7 @@ const Footer = () => {
             <button>btn 2</button>
             <button>this is button 3</button>
             <button>button 4</button>
-            <button>buttn 5</button>
-            <button>btn 6</button>
+            <button>btn</button>
 
         </div>
     )
